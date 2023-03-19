3d861ad3c4fdcfea0f3b194e1308043bf66f7ffb
Rifat Metehan GOKTAS <metehangoktas01@gmail.com>
2023-03-20 02:56:59 +0300

    test

--- lib/helpers/utils.dart
+++ lib/helpers/utils.dart
@@ -13,7 +13,7 @@ class Utils {
           subExercises: [
             SubExercise(
               color: const Color(0xFF000000),
-              name: "Spartan Push Up",
+              name: "Spartann Push Up",
               imgName: " ",
               subExercises: []
               ),
