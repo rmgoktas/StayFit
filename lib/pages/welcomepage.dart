4fc9b92f9bbc3114f988a366e8a82249789c8565
Rifat Metehan GOKTAS <metehangoktas01@gmail.com>
2023-03-24 02:54:06 +0300

    added onboardpage button

--- lib/pages/welcomepage.dart
+++ lib/pages/welcomepage.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:stayfit_app/pages/exerciselistpage.dart";
+import "package:stayfit_app/pages/onboardpage.dart";
 
 class WelcomePage extends StatelessWidget {
   @override
@@ -10,10 +11,13 @@ class WelcomePage extends StatelessWidget {
             child: Stack(children: [
               Positioned.fill(
                   child: Opacity(
-                opacity: 0.3,
-                child:
-                    Image.asset("assets/welcomeback.jpg", fit: (BoxFit.cover)),
-              )),
+                  opacity: 0.3,
+                  child: Image.asset(
+                    "assets/welcomeback.jpg", 
+                    fit: (BoxFit.cover)
+                  ),
+                )
+              ),
               Center(
                   child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -21,17 +25,17 @@ class WelcomePage extends StatelessWidget {
                       children: [
                     Center(
                       child: ClipOval(
-                          child: Container(
-                        width: 180,
-                        height: 180,
-                        color: Color.fromARGB(120, 255, 255, 255),
-                        alignment: Alignment.center,
-                        child: Image.asset(
-                          "assets/blackicon.png",
-                        ),
-                      )
+                        child: Container(
+                          width: 180,
+                          height: 180,
+                          color: Color.fromARGB(120, 255, 255, 255),
+                          alignment: Alignment.center,
+                          child: Image.asset(
+                            "assets/blackicon.png",
+                          ),
+                        )
+                      ),
                     ),
-                  ),
                     SizedBox(height: 15),
                     Text(
                       "Welcome !",
@@ -45,34 +49,77 @@ class WelcomePage extends StatelessWidget {
                     Text(
                       "Start the change today.",
                       textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white, fontSize: 15),
+                      style: TextStyle(
+                        color: Colors.white, 
+                        fontSize: 15),
                     ),
                     SizedBox(height: 50),
                     Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: MaterialButton(
-                            onPressed: () {
+                      padding: const EdgeInsets.all(20),
+                      child: MaterialButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                            builder: (context) => OnboardPage()
+                            )
+                          );
+                        },
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)
+                        ),
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        padding: const EdgeInsets.all(25),
+                        child: Text("Start Onboarding",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                           )
+                         )
+                       )
+                     ),
+                    Container(
+                      margin: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            splashColor: Color.fromARGB(255, 255, 255, 255).withOpacity(0.2),
+                            highlightColor: Color.fromARGB(255, 255, 255, 255).withOpacity(0.2),
+                            onTap: () {
                               Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => ExerciseListPage()
-                                    )
-                                );
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => ExerciseListPage()
+                                )
+                              );
                             },
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50)
-                            ),
-                            color: Color.fromARGB(255, 255, 255, 255),
-                            padding: const EdgeInsets.all(25),
-                            child: Text("Start Now",
+                            child: Container(
+                              padding: const EdgeInsets.all(20),
+                              child: Text(
+                                "Start Now",
+                                textAlign: TextAlign.center,
                                 style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold
-                                    )
-                                  )
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.transparent,
+                                border: Border.all(
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                  width: 4
                                 )
                               ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
                     Container(
                       margin: EdgeInsets.only(left: 20, right: 20, bottom: 20),
                       child: ClipRRect(
